3c726c61509bc97e6fda490e24078021b886a813
Andre Silva <andreswebs@pm.me>
2026-03-11 14:08:23 -0300

    init
    Signed-off-by: Andre Silva <andreswebs@pm.me>

--- hooks/post_gen_project.py
+++ hooks/post_gen_project.py
@@ -2,23 +2,6 @@ import os
 import shutil
 import subprocess
 
-SENTINEL = '__REPLACE_ME_'
-
-values = {
-    'project_name_full': '{{ cookiecutter.project_name_full }}',
-    'tfstate_bucket': '{{ cookiecutter.tfstate_bucket }}',
-    'tfstate_kms_key_name': '{{ cookiecutter.tfstate_kms_key_name }}',
-    'aws_account_alias_dev': '{{ cookiecutter.aws_account_alias_dev }}',
-    'aws_account_alias_live': '{{ cookiecutter.aws_account_alias_live }}',
-    'aws_account_alias_shared': '{{ cookiecutter.aws_account_alias_shared }}',
-    'iam_role_arn': '{{ cookiecutter.iam_role_arn }}',
-}
-
-unreplaced = [k for k, v in values.items() if v.startswith(SENTINEL)]
-if unreplaced:
-    print(f"Error: The following values still contain placeholder defaults: {', '.join(unreplaced)}")
-    exit(1)
-
 {% if cookiecutter.git_init -%}
 try:
     subprocess.run(['git', 'init'], check=True)
